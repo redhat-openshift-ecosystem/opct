4da1d3c11da7e78beb1408db21e5cca83d3f9506
Robert Bost <rbost@redhat.com>
2022-05-26 21:07:31 -0600

    feat(hack): add add fmt and lint

---
+++ hack/go-imports.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Example: ./hack/go-imports.sh pkg/
+if [ "$IS_CONTAINER" != "" ]; then
+  go install golang.org/x/tools/cmd/goimports@latest
+  for TARGET in "${@}"; do
+    find "${TARGET}" -name '*.go' ! -name 'bindata.go' ! -path '*/vendor/*' ! -path '*/.build/*' -exec goimports -w {} \+
+  done
+  git diff --exit-code
+else
+  podman run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/go/src/github.com/openshift/provider-certification-tool:z" \
+    --workdir /go/src/github.com/openshift/provider-certification-tool \
+    docker.io/golang:1.17 \
+    ./hack/go-imports.sh "${@}"
+fi
---
+++ hack/go-staticcheck.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Example:  ./hack/go-staticcheck.sh ./...
+
+if [ "$IS_CONTAINER" != "" ]; then
+    go install honnef.co/go/tools/cmd/staticcheck@latest
+    staticcheck "${@}"
+else
+  podman run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/go/src/github.com/openshift/provider-certification-tool:z" \
+    --workdir /go/src/github.com/openshift/provider-certification-tool \
+    docker.io/golang:1.17 \
+    ./hack/go-staticcheck.sh "${@}"
+fi
