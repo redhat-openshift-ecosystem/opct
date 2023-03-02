d707c9aa4d1f8b204e5ca54085aac89a7ed4f7fd
Julio Faerman <jufaerma@redhat.com>
2023-03-02 15:46:16 +0000

    wip

--- scripts/test-cluster.sh
+++ scripts/test-cluster.sh
@@ -2,25 +2,31 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Testing cluster [$1] [$2] [$3] [$4]"
 ocp_version=$1
 ocpt_version=$2
-provider=$3
-profile=$4
+profile=$3
+echo "- Testing cluster ocp_version[$1] ocpt_version[$2] profile[$3] --"
+
 
 # prepare cluster
 timestamp=$(date +'%H%M%S')
 username=$USER
 cluster_name="$username$timestamp"
-echo "---- cluster name: $cluster_name"
+echo "-- Cluster name: $cluster_name"
 
 cluster_home="/tmp/clusters"
 cluster_dir="$cluster_home/$cluster_name"
 mkdir -p "$cluster_dir"
-echo "Cluster dir: $cluster_dir"
+echo "-- Cluster dir: $cluster_dir"
 
-# render template
-template_dir="$DIR/templates/$provider/$profile"
+template_dir="$DIR/templates/$ocp_version/$profile"
+if [ ! -d "$template_dir" ]; then
+  echo "Template directory does not exist: $template_dir"
+  exit 1
+fi
+echo "-- Template dir: $template_dir"
+
+exit 0
 template_file="$template_dir/install-config.j2.yaml"
 data_file="$template_dir/data.yaml"
 
