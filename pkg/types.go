ccfc7d3e1c38411d61ee9efd51d3905e1a6ca4a4
Marco Braga <braga@mtulio.eng.br>
2024-12-28 03:29:54 -0300

    OPCT-000: Bump to v0.5.1 to address bug fixes

--- pkg/types.go
+++ pkg/types.go
@@ -19,9 +19,9 @@ const (
 	SonobuoyLabelComponentName     = "component"
 	SonobuoyLabelComponentValue    = "sonobuoy"
 	DefaultToolsRepository         = "quay.io/opct"
-	PluginsImage                   = "plugin-openshift-tests:v0.5.0"
-	CollectorImage                 = "plugin-artifacts-collector:v0.5.0"
-	MustGatherMonitoringImage      = "must-gather-monitoring:v0.5.0"
+	PluginsImage                   = "plugin-openshift-tests:v0.5.1"
+	CollectorImage                 = "plugin-artifacts-collector:v0.5.1"
+	MustGatherMonitoringImage      = "must-gather-monitoring:v0.5.1"
 	OpenShiftTestsImage            = "image-registry.openshift-image-registry.svc:5000/openshift/tests"
 )
 
