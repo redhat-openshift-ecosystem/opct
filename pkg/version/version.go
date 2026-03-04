970723d2c3a02dc4f84b4966cd8787d515931629
Marco Braga <mrbraga@redhat.com>
2026-03-04 11:20:53 -0300

    NO-JIRA: enhance version command with cluster version
    Enhance version command with more details about the environment,
    including plugins image versions and cluster version.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,8 +4,10 @@ package version
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/redhat-openshift-ecosystem/opct/pkg"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/vmware-tanzu/sonobuoy/pkg/buildinfo"
 )
@@ -29,21 +31,40 @@ type VersionContext struct {
 }
 
 func (vc *VersionContext) String() string {
-	return fmt.Sprintf("OPCT CLI: %s+%s", vc.Version, vc.Commit)
+	if vc.Version == "0.0.0" {
+		return fmt.Sprintf("OPCT CLI: %s+%s", vc.Version, vc.Commit)
+	}
+	return fmt.Sprintf("OPCT CLI: %s", vc.Version)
 }
 
-func (vc *VersionContext) StringPlugins() string {
-	return fmt.Sprintf("OPCT Plugins: %s", pkg.PluginsImage)
+func (vc *VersionContext) stringPluginsImage() {
+	fmt.Printf("Images versions:")
+	fmt.Printf("\n %s (library %s)", pkg.SonobuoyImage, buildinfo.Version)
+	fmt.Printf("\n %s", pkg.PluginsImage)
+	fmt.Printf("\n %s", pkg.CollectorImage)
+	fmt.Printf("\n %s", pkg.MustGatherMonitoringImage)
+	fmt.Println("")
 }
 
 func NewCmdVersion() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
-		Short: "Print provider validation tool version",
+		Short: "Print opct CLI version",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println(Version.String())
-			fmt.Println(Version.StringPlugins())
-			fmt.Printf("Sonobuoy: %s\n", buildinfo.Version)
+			Version.stringPluginsImage()
+			// get cluster version if KUBECONFIG is set
+			clusterVersion, err := GetClusterVersion()
+			if err != nil {
+				if err.Error() == "KUBECONFIG is not set" {
+					os.Exit(0)
+				}
+				log.WithError(err).Error("failed when getting cluster version")
+				os.Exit(1)
+			}
+			fmt.Println("")
+			fmt.Printf(" OpenShift: %s\n", clusterVersion.OpenShift)
+			fmt.Printf(" Kubernetes: %s\n", clusterVersion.Kubernetes)
 		},
 	}
 }
